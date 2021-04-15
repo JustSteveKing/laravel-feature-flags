9572feb69a24a06552db4ee27b2916fb3649c15d
Steve McDougall <juststevemcd@gmail.com>
2021-04-15 08:53:50 +0100

    Creating base Test Case for the package

---
+++ tests/TestCase.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JustSteveKing\Laravel\FeatureFlags\Tests;
+
+use JustSteveKing\Laravel\FeatureFlags\FeatureFlagsServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
+
+class TestCase extends Orchestra
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            FeatureFlagsServiceProvider::class,
+        ];
+    }
+
+    public function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('database.default', 'sqlite');
+        $app['config']->set('database.connections.sqlite', [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ]);
+    }
+}
