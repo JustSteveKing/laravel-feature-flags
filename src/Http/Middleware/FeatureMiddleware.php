61a042ad2564c8dd4f1adc08edcb91da363027dc
Steve McDougall <juststevemcd@gmail.com>
2021-04-15 13:45:41 +0100

    DX for passing through args to middleware

--- src/Http/Middleware/FeatureMiddleware.php
+++ src/Http/Middleware/FeatureMiddleware.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace JustSteveKing\Laravel\FeatureFlags\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
 class FeatureMiddleware
@@ -12,7 +13,7 @@ class FeatureMiddleware
     public function handle(Request $request, Closure $next, string ...$features): mixed
     {
         foreach ($features as $feature) {
-            if (! auth()->user()->hasFeature($feature)) {
+            if (! auth()->user()->hasFeature(Str::replaceFirst('-', ' ', $feature))) {
                 return redirect()->back();
             }
         }
