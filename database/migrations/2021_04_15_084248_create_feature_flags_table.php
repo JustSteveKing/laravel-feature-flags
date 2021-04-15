63e47eeac05742be509953eaf9315213d4eb35e0
Steve McDougall <juststevemcd@gmail.com>
2021-04-15 09:53:00 +0100

    Fixing migrations for package

--- database/migrations/2021_04_15_084248_create_feature_flags_table.php
+++ database/migrations/2021_04_15_084248_create_feature_flags_table.php
@@ -4,14 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFeaturesTable extends Migration
+class CreateFeatureFlagsTable extends Migration
 {
     public function up()
     {
         Schema::create('features', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->text('description');
+            $table->text('description')->nullable();
             $table->boolean('active')->default(true);
             $table->timestamps();
         });
