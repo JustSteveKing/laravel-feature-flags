36f838fc58e5b3081a3c93475cdab8449f54aeec
Steve McDougall <juststevemcd@gmail.com>
2021-04-15 11:58:16 +0100

    Fix error in migration

--- database/migrations/2021_04_15_094286_create_feature_group_user_table.php
+++ database/migrations/2021_04_15_094286_create_feature_group_user_table.php
@@ -20,7 +20,7 @@ class CreateFeatureGroupUserTable extends Migration
             $table->foreign('feature_group_id')->references('id')->on('feature_groups')->onDelete('CASCADE');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('CASCADE');
 
-            $table->primary(['feature_id', 'user_id']);
+            $table->primary(['feature_group_id', 'user_id']);
         });
     }
 
