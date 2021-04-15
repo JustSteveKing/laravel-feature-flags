298ae7823293d306de988c98039fc40315ae0b77
Steve McDougall <juststevemcd@gmail.com>
2021-04-15 10:21:43 +0100

    Feature Group Model

---
+++ database/migrations/2021_04_15_094248_create_feature_groups_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFeatureGroupsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('feature_groups', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('features');
+    }
+}
