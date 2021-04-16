3b9efc08a75f5c07d095f473770f2096ad10a3ee
Mark Railton <marksrailton@gmail.com>
2021-04-16 09:29:42 +0100

    Inline checks and also make sure feature is not already in group before adding

--- src/Console/AddFeatureToGroup.php
+++ src/Console/AddFeatureToGroup.php
@@ -17,25 +17,29 @@ class AddFeatureToGroup extends Command
     public function handle()
     {
         $featureName = $this->ask('Feature Name');
-        $groupName = $this->ask('Group Name');
-
         $feature = Feature::name($featureName)->first();
-        $group = FeatureGroup::name($groupName)->first();
 
         if (!$feature) {
             $this->alert("There is no feature with the name {$featureName}");
+            $featureName = $this->ask('Feature Name');
+            $feature = Feature::name($featureName)->first();
         }
+
+        $groupName = $this->ask('Group Name');
+        $group = FeatureGroup::name($groupName)->first();
 
         if (!$group) {
             $this->alert("There is no group with the name {$groupName}");
+            $groupName = $this->ask('Group Name');
+            $group = FeatureGroup::name($groupName)->first();
         }
 
-        if (!$feature || !$group) {
+        if ($group->hasFeature($feature->name)) {
+            $this->alert("This feature is already assigned to this group");
             return 1;
         }
 
         $group->addFeature($feature);
-
         $this->info("Added feature '{$featureName}' to group '{$groupName}'");
 
         return 0;
