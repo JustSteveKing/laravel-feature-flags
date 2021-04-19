4fa2c4fa4ca4bd2cd5a82c5e31a6840b2ebf2049
Steve McDougall <juststevemcd@gmail.com>
2021-04-19 08:39:23 +0100

    Adding trait to test model

--- tests/Stubs/User.php
+++ tests/Stubs/User.php
@@ -5,9 +5,12 @@ declare(strict_types=1);
 namespace JustSteveKing\Laravel\FeatureFlags\Tests\Stubs;
 
 use Illuminate\Foundation\Auth\User as AuthUser;
+use JustSteveKing\Laravel\FeatureFlags\Concerns\HasFeatures;
 
 class User extends AuthUser
 {
+    use HasFeatures;
+    
     public $guarded = [];
 
     public $table = 'users';
