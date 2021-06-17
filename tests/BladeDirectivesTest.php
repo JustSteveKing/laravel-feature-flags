fc12f9c95582c6ca37975492d087acf5aa875a52
thinkverse <hallberg.kim@gmail.com>
2021-06-17 12:30:49 +0200

    Add tests for blade directives

---
+++ tests/BladeDirectivesTest.php
@@ -0,0 +1,92 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JustSteveKing\Laravel\FeatureFlags\Tests;
+
+use Illuminate\Foundation\Testing\Concerns\InteractsWithViews;
+use JustSteveKing\Laravel\FeatureFlags\Models\FeatureGroup;
+use JustSteveKing\Laravel\FeatureFlags\Tests\Stubs\User;
+use JustSteveKing\Laravel\FeatureFlags\Models\Feature;
+use Illuminate\Support\Facades\Hash;
+
+class BladeDirectivesTest extends TestCase
+{
+    use InteractsWithViews;
+
+    /**
+     * @test
+     */
+    public function it_shows_feature_to_user_that_have_feature()
+    {
+        $user = User::create([
+            'name' => 'test user',
+            'email' => 'test@user.com',
+            'password' => Hash::make('password')
+        ]);
+
+        $feature = Feature::create([
+            'name' => 'test'
+        ]);
+
+        $user->giveFeature($feature->name);
+
+        $view = $this->actingAs($user)->blade("@feature('test') active feature @endfeature");
+
+        $view->assertSee('active feature');
+    }
+
+    /**
+     * @test
+     */
+    public function it_shows_feature_to_user_in_feature_group()
+    {
+        $user = User::create([
+            'name' => 'test user',
+            'email' => 'test@user.com',
+            'password' => Hash::make('password')
+        ]);
+
+        $group = FeatureGroup::create([
+            'name' => 'Test Group',
+        ]);
+
+        $user->joinGroup($group->name);
+
+        $view = $this->actingAs($user)->blade("@featuregroup('test group') active feature @endfeaturegroup");
+
+        $view->assertSee('active feature');
+    }
+
+    /**
+     * @test
+     */
+    public function it_hides_feature_to_user_that_dont_have_feature()
+    {
+        $user = User::create([
+            'name' => 'test user',
+            'email' => 'test@user.com',
+            'password' => Hash::make('password')
+        ]);
+
+        $view = $this->actingAs($user)->blade("@feature('test') active feature @endfeature");
+
+        $view->assertSee('');
+    }
+
+    /**
+     * @test
+     */
+    public function it_hides_feature_to_user_not_in_feature_group()
+    {
+        $user = User::create([
+            'name' => 'test user',
+            'email' => 'test@user.com',
+            'password' => Hash::make('password')
+        ]);
+
+        $view = $this->actingAs($user)->blade("@featuregroup('test group') active feature @endfeaturegroup");
+
+        $view->assertSee('');
+    }
+}
