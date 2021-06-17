f856dd1faae3132464059fa67ae18ef5865cbb38
thinkverse <hallberg.kim@gmail.com>
2021-06-18 00:15:07 +0200

    Add tests for groupfeature blade directive

--- tests/BladeDirectivesTest.php
+++ tests/BladeDirectivesTest.php
@@ -61,6 +61,34 @@ class BladeDirectivesTest extends TestCase
     /**
      * @test
      */
+    public function it_shows_feature_to_user_when_feature_group_has_feature()
+    {
+        $user = User::create([
+            'name' => 'test user',
+            'email' => 'test@user.com',
+            'password' => Hash::make('password')
+        ]);
+
+        $feature = Feature::create([
+            'name' => 'test'
+        ]);
+
+        $group = FeatureGroup::create([
+            'name' => 'Test Group',
+        ]);
+
+        $group->addFeature($feature);
+
+        $user->joinGroup($group->name);
+
+        $view = $this->actingAs($user)->blade("@groupfeature('test') active feature @endgroupfeature");
+
+        $view->assertSee('active feature');
+    }
+
+    /**
+     * @test
+     */
     public function it_hides_feature_to_user_that_dont_have_feature()
     {
         $user = User::create([
@@ -86,6 +114,28 @@ class BladeDirectivesTest extends TestCase
         ]);
 
         $view = $this->actingAs($user)->blade("@featuregroup('test group') active feature @endfeaturegroup");
+
+        $view->assertSee('');
+    }
+
+    /**
+     * @test
+     */
+    public function it_hides_feature_to_user_when_feature_group_dont_have_feature()
+    {
+        $user = User::create([
+            'name' => 'test user',
+            'email' => 'test@user.com',
+            'password' => Hash::make('password')
+        ]);
+
+        $group = FeatureGroup::create([
+            'name' => 'Test Group',
+        ]);
+
+        $user->joinGroup($group->name);
+
+        $view = $this->actingAs($user)->blade("@groupfeature('test') active feature @endgroupfeature");
 
         $view->assertSee('');
     }
